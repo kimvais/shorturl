18d46777b24670fca4d2685be1aaedea40badd10
Kimmo Parviainen-Jalanko <kimvais@ssh.com>
2014-08-27 14:56:02 +0300

    Merged

--- conv.py
+++ conv.py
@@ -1,10 +1,33 @@
 import sqlite3
 import logging
 
+from boto.dynamodb2.fields import HashKey
+from boto.dynamodb2.items import Item
+from boto.dynamodb2.table import Table
+from boto.exception import JSONResponseError
+
+from shorturl.utils import itou
+
+
 logger = logging.getLogger(__name__)
 
+
 def main():
-    pass
+    c = sqlite3.connect('short_urls.sqlite3').cursor()
+    c.execute('SELECT id, url, clicks, created FROM shorturl_url')
+    try:
+        urls = Table.create('short_urls', schema=[HashKey('url_id'), ])
+    except JSONResponseError:
+        urls = Table('short_urls')
+
+    keys = list()
+    for row in c.fetchall():
+        num_id, url, clicks, created = row
+        logger.info('ID: {}({}) {} / {} ({})'.format(itou(num_id), num_id, url, clicks, created))
+        key = itou(num_id)
+        keys.append(num_id)
+        uo = Item(urls, data=dict(url_id=key, id_n=num_id, url=url, clicks=clicks, created=created))
+        uo.save(overwrite=False)
 
 
 if __name__ == '__main__':
